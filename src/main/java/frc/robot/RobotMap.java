c07e5d60a03ac6ad55ef5448597b990bcec72b19
Samrat Sahoo <samratsahoo2013@gmail.com>
2020-01-07 19:45:15 -0600

    Added Joystick constants

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,5 +2,25 @@ package frc.robot;
 
 public class RobotMap{
 
+    //JOYSTICKS
+    public static final int JOYSTICK = 0;
+
+    //JOYSTICK AXES
+    public static final int LEFT_X_AXIS = 0;
+    public static final int LEFT_Y_AXIS = 1;
+    public static final int RIGHT_X_AXIS = 4;
+    public static final int RIGHT_Y_AXIS = 5;
+
+    //BUTTONS
+    public static final int A_BUTTON = 1;
+    public static final int B_BUTTON = 2;
+    public static final int X_BUTTON = 3;
+    public static final int Y_BUTTON = 4;
+    public static final int LB_BUTTON = 5;
+    public static final int RB_BUTTON = 6;
+    public static final int GO_BACK_BUTTON = 7;
+    public static final int START_BUTTON = 8;
+    public static final int CLICK_LEFT_JOYSTICK = 9;
+    public static final int CLICK_RIGHT_JOYSTICK = 10;
 
 }
