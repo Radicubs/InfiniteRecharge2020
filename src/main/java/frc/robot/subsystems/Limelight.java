5fc0174b7ee105e9efb314a0a0ef825c00a7427b
Samrat Sahoo <samratsahoo2013@gmail.com>
2020-01-21 18:09:20 -0600

    Added Vision Code

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,39 +1,78 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Limelight extends Subsystem {
 
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    double ledState = 0.0;
+  double ledState;
+  NetworkTable table;
+  NetworkTableEntry tx;
+  NetworkTableEntry ty;
+  NetworkTableEntry ta;
+  double horizonatalOffset;
+  double verticalOffset;
+  double targetArea;
 
-    public Limelight(){
-      
-    }
+  public Limelight() {
+    // Instantiate Network Table + Entries
+    table = NetworkTableInstance.getDefault().getTable("limelight");
+    tx = table.getEntry("tx");
+    ty = table.getEntry("ty");
+    ta = table.getEntry("ta");
 
-    public void setLedOn() {
+    // Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees)
+    horizonatalOffset = tx.getDouble(0.0);
 
-        ledState = 3.0;  
-      }
-    
-      public void setLedOff() {
-    
-        ledState = 1.0;
-      }
-    
-      public void setLedBlink() {
-    
-        ledState = 2.0; 
-      }
+    // Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
+    verticalOffset = ty.getDouble(0.0);
 
-    @Override
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        // setDefaultCommand(new MySpecialCommand());
+    // Target Area (0% of image to 100% of image)
+    targetArea = ta.getDouble(0.0);
 
-        // setDefaultCommand(new UpdateLimeLight()); }
-    }
+    // Sets LED on at startup
+    setLedOn();
+
+  }
+
+  public void setLedOn() {
+    // 3.0 = LED ON
+    ledState = 3.0;
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(ledState);
+  }
+
+  public void setLedOff() {
+    // 1.0 = LED OFF
+    ledState = 1.0;
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(ledState);
+  }
+
+  public void setLedBlink() {
+    // 2.0 = LED BLINK
+    ledState = 2.0;
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(ledState);
+  }
+
+  public double gethorizonatalOffset() {
+    return this.horizonatalOffset;
+  }
+
+  public double getverticalOffset() {
+    return this.horizonatalOffset;
+  }
+
+  public double getTargetArea() {
+    return this.targetArea;
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+
+    // setDefaultCommand(new UpdateLimeLight()); }
+  }
 
 }
