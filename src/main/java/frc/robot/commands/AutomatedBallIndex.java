87ea77e3d328d872e821cce3d6c518fcc7bdeae4
Samrat Sahoo <samratsahoo2013@gmail.com>
2020-03-02 20:12:09 -0600

    Added Automated Ball Indexing

---
+++ src/main/java/frc/robot/commands/AutomatedBallIndex.java
@@ -0,0 +1,41 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class AutomatedBallIndex extends Command{
+
+  public AutomatedBallIndex() {
+    requires(Robot.colorSensor);
+    requires(Robot.index);
+  }
+
+  // Time to Run command (use it for distance as well)
+
+  @Override
+  protected void initialize() {
+      } 
+
+@Override
+  protected void execute() {
+      if(Robot.colorSensor.foundBall()){
+        Robot.index.index(0.7);
+      }
+  }
+
+  @Override
+  protected boolean isFinished() {
+    // Make this return true when this Command no longer needs to run execute()
+    return false;
+  }
+
+  @Override
+  protected void end() {
+    Robot.driveBase.drive(0,0);
+  }
+
+  @Override
+  protected void interrupted() {
+    super.interrupted();
+  }
+}
--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -7,13 +7,14 @@ import com.revrobotics.ColorMatchResult;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.util.Color;
+import frc.robot.commands.AutomatedBallIndex;
 
 public class ColorSensor extends Subsystem {
 
   I2C.Port i2cPort = I2C.Port.kOnboard;
   ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
   private final ColorMatch colorMatcher = new ColorMatch();
-  private final Color yellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113); //Needs Calibration
+  private final Color yellowTarget = ColorMatch.makeColor(0.154, 0.502, 0.154); //Needs Calibration
   Color detectedColor = colorSensor.getColor();
   ColorMatchResult match;
 
@@ -24,12 +25,10 @@ public class ColorSensor extends Subsystem {
     colorSensor = new ColorSensorV3(i2cPort);
   }
 
-  public Color getColor() {
-    return colorSensor.getColor();
-  }
   
   public boolean foundBall() {
      match = colorMatcher.matchClosestColor(detectedColor);
+     System.out.println(match.color == yellowTarget);
      return match.color == yellowTarget;
   }
 
@@ -37,6 +36,7 @@ public class ColorSensor extends Subsystem {
   public void initDefaultCommand() {
     // Default command for limelight subsystem here.
 
-    // setDefaultCommand(new UpdateLimelight());
+      setDefaultCommand(new AutomatedBallIndex());
   }
+
 }
