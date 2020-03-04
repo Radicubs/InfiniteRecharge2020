ec4bb7e4c7966610d2a9591c9b586a7bc69ef035
Samrat Sahoo <samratsahoo2013@gmail.com>
2020-03-03 20:00:37 -0600

    Fixed errors

--- src/main/java/frc/robot/commands/AutoShoot.java
+++ src/main/java/frc/robot/commands/AutoShoot.java
@@ -18,13 +18,13 @@ public class AutoShoot extends CommandGroup {
     // TO GET READY FOR SHOOTING BASED
     // ON times[]
     addSequential(new IndexBall(0.5))
-    addSequential(new WaitCommand(times[balls]));
+    addSequential(new WaitCommand(times[balls])); // Inversely proportional constant
     addSequential(new IndexBall(0));
 
     // WE NEED TO CALCULATE THE TIMEOUT FOR X NUM OF BALLS
     addSequential(new RotationAlignment());  
     addSequential(new DistanceAlignment());
-    addSequential(new DynamicShooting(), timeout);
+    addSequential(new DynamicShooting(), balls); // directly proportional constant
   }
 
   public AutoShoot() {
